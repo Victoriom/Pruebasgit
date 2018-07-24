0a98d88d88c57b27a9a6d116b6318a865795c558
Victor Gomis Palazon <vicgompal@gmail.com>
2018-07-24 22:50:33 +0200

    Se da funcionalidad al boton del activity

--- app/src/main/java/com/vic/vic/pruebagit/MainActivity.java
+++ app/src/main/java/com/vic/vic/pruebagit/MainActivity.java
@@ -2,6 +2,7 @@ package com.vic.vic.pruebagit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void click(View view){
+
+    }
+
 }
